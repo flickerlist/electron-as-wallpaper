b8407159c03b0c805c1ff02b1c8ca7a3c34d6bd7
lvxiaobao <lvxiaobao_fbi@163.com>
2024-03-16 11:59:06 +0800

    add dist to repo

---
+++ dist/main.d.ts
@@ -0,0 +1,33 @@
+import { BrowserWindow } from 'electron';
+
+interface AttachOptions {
+    /**
+     * Makes the window transparent.
+     * @default false
+     */
+    transparent?: boolean;
+    /**
+     * Forward mouse input-forwarding to the window.
+     * @default false
+     */
+    forwardMouseInput?: boolean;
+    /**
+     * Forward keyboard input-forwarding to the window.
+     * @default false
+     */
+    forwardKeyboardInput?: boolean;
+}
+/**
+ * Set window behind desktop icons
+ */
+declare const attach: (win: BrowserWindow, options?: AttachOptions) => void;
+/**
+ * Remove a window from desktop icons
+ */
+declare const detach: (win: BrowserWindow) => void;
+/**
+ * Refresh desktop
+ */
+declare const refresh: () => void;
+
+export { type AttachOptions, attach, detach, refresh };
